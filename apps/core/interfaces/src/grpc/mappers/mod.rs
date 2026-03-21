d3c82cb1c17cd12c790280ae1c6ba9efae842661
Elwqnn <elwann.guillemot@epitech.eu>
2026-03-21 05:09:33 +0100

    fix: list pipelines don't return node details anymore

--- apps/core/interfaces/src/grpc/mappers/mod.rs
+++ apps/core/interfaces/src/grpc/mappers/mod.rs
@@ -11,6 +11,6 @@ pub use error_mapper::domain_error_to_status;
 pub use job_mapper::job_to_proto;
 pub use organization_mapper::organization_to_proto;
 pub use pagination_mapper::{domain_to_proto_metadata, proto_to_domain_pagination};
-pub use pipeline_mapper::pipeline_to_proto;
+pub use pipeline_mapper::{pipeline_node_to_proto, pipeline_to_proto, pipeline_to_proto_summary};
 pub use project_mapper::project_to_proto;
 pub use user_mapper::user_to_proto;
