eafe84577bb0b87216cf60941bf0ab227eed3376
RemFdz <114572880+RemFdz@users.noreply.github.com>
2026-06-26 00:25:37 +0900

    refactor(frontend): refactor naming convention to separate utility interfaces for object typing than domain entities (#128)

--- apps/frontend/src/modules/features/pipeline/presentation/ui/dashboard/pipeline-table/PipelineStatus.tsx
+++ apps/frontend/src/modules/features/pipeline/presentation/ui/dashboard/pipeline-table/PipelineStatus.tsx
@@ -1,6 +1,8 @@
-import StatusIndicator, { type StatusState } from '@shared/presentation/ui/data-display/status-indicator.tsx';
+import StatusIndicator, {
+  type StatusState,
+} from '@shared/presentation/ui/data-display/status-indicator.tsx';
 import { Trans } from '@lingui/react/macro';
-import type { PipelineMetadata } from '@/modules/features/pipeline/domain/models/pipeline.model.ts';
+import type { PipelineMetadata } from '@/modules/features/pipeline/domain/structs/pipeline.struct.ts';
 
 type PipelineStatusProps = {
   pipeline: PipelineMetadata;
